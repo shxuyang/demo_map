b80faaebf2152c39b9f1bd536e7fcb8352b79b19
shxuyang <xuyang_love88@163.com>
2024-09-24 18:21:07 +0800

    Update FoodTruckApplicationTests.java

--- EsteeLauder/src/test/java/com/example/FoodTruckApplicationTests.java
+++ EsteeLauder/src/test/java/com/example/FoodTruckApplicationTests.java
@@ -1,1 +1,19 @@
+package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTruckApplicationTests {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void testGetAllFoodTruck() {
+    }
+
+    //...
+}
